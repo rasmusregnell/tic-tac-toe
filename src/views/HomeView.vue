2540fe5adb9ef211d8d029b237934292e372e54e
rasmusregnell <rasmuserik2009@gmail.com>
2024-05-30 00:03:35 +0200

    basis game done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -45,12 +45,12 @@ function check_has_won(board: boolean[][]): boolean {
   return false;
 }
 
-function get_possible_moves() {
+function get_possible_moves(): Array<Array<number>> {
   const possible_moves = [];
-  for (let x = 0; x < 3; x++) {
-    for (let y = 0; y < 3; y++) {
-      if (!player.value[x][y] && !computer.value[x][y])
-        possible_moves.push([x, y]);
+  for (let y = 0; y < 3; y++) {
+    for (let x = 0; x < 3; x++) {
+      if (!player.value[y][x] && !computer.value[y][x])
+        possible_moves.push([y, x]);
     }
   }
   return possible_moves;
@@ -63,36 +63,52 @@ function computer_move() {
   computer.value[index[0]][index[1]] = true;
 }
 
-function check_condition(actor: Array<Array<boolean>>) {
-  if (check_has_won(actor)) {
-    has_won.value = true;
-  } else if (get_possible_moves().length == 0) {
-    tie.value = true;
-  } else {
+function arraysEqual(a1: number[], a2: number[]): boolean {
+  if (a1.length !== a2.length) {
     return false;
+  }
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
   }
   return true;
 }
 
+//returns true if game is done, updates values on game state
+function check_condition(actor: Array<Array<boolean>>, player: boolean) {
+  if (check_has_won(actor) && player) {
+    has_won.value = true;
+    return true;
+  }
+  if (check_has_won(actor) && !player) {
+    has_lost.value = true;
+    return true;
+  }
+  if (get_possible_moves().length == 0) {
+    tie.value = true;
+    return true;
+  }
+  return false;
+}
+
 function round(x: number, y: number) {
   //player makes move
-  player.value[y - 1][x - 1] = true;
-  if (check_has_won(player.value)) {
-    has_won.value = true;
-  } else if (get_possible_moves().length == 0) {
-    tie.value = true;
-  } else {
-    setTimeout(() => {
-      clickable.value = true;
-      computer_move();
-    }, 1000);
-    if (check_has_won(computer.value)) {
-      has_lost.value = true;
-    } else if (get_possible_moves().length == 0 && !has_won) {
-      tie.value = true;
-    }
-    clickable.value = false;
+  if (!get_possible_moves().find((e) => arraysEqual(e, [y - 1, x - 1]))) {
+    console.log("hej");
+    return;
   }
+  //console.log(get_possible_moves());
+  player.value[y - 1][x - 1] = true;
+  if (check_condition(player.value, true)) {
+    return;
+  }
+  clickable.value = false;
+  setTimeout(() => {
+    computer_move();
+    check_condition(computer.value, false);
+    clickable.value = true;
+  }, 1000);
 }
 
 function restart() {
@@ -130,7 +146,7 @@ function restart() {
         <div
           v-for="x in 3"
           @click="round(x, y)"
-          :class="`w-1/3 h-full border-[#000000] border-2 flex justify-center items-center ${clickable ? 'pointer-events-auto' : 'pointer-events-none'}`"
+          :class="`w-1/3 h-full border-[#000000] border-2 flex justify-center items-center ${has_won || has_lost || !clickable ? 'pointer-events-none' : 'pointer-events-auto'}`"
         >
           <div
             v-if="player[y - 1][x - 1]"
@@ -144,7 +160,11 @@ function restart() {
       </div>
     </div>
     <div class="w-full flex justify-center">
-      <button v-if="has_won" @click="restart" class="text-2xl text-dark_green">
+      <button
+        v-if="has_won || has_lost || tie"
+        @click="restart"
+        class="text-2xl text-dark_green"
+      >
         Play again
       </button>
     </div>
