f4222779eb924af9f4bddc64e67d8b0439dc38ba
rasmusregnell <rasmuserik2009@gmail.com>
2024-05-30 01:12:06 +0200

    updated routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import PlayView from "../views/PlayView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/home',
-      name: 'home',
-      component: HomeView
+      path: "/play",
+      name: "play",
+      component: PlayView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/home",
+      name: "home",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      component: () => import("../views/HomeView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
